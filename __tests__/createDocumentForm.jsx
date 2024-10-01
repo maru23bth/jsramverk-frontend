a4dc90d5a18c3cd58e1059d9f1e8ed62fbd3d4ae
Olha Bruce <olbr22@student.bth.se>
2024-10-01 13:05:10 +0200

    Remove unused import

--- __tests__/createDocumentForm.jsx
+++ __tests__/createDocumentForm.jsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom'
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event';
 import CreateDocumentForm from '@/app/components/createDocumentForm'
 import { createDocument } from '@/app/apiRequests'; // The real function
 
