376be37fea1e99dc8e99c74eb88a32046c6215a4
Olha Bruce <olbr22@student.bth.se>
2024-10-14 12:53:53 +0200

    Add socket-client

--- app/(dashboard)/document/edit/editDocument.js
+++ app/(dashboard)/document/edit/editDocument.js
@@ -1,24 +1,53 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { TextField, Box, Typography } from '@mui/material';
 import CustomizedMenuBtn from './optionsMenuBtn';
 import { fetchDocument } from '@/app/apiRequests';
+// socket
+import { io } from "socket.io-client";
 
 export default function EditDocument() {
     const searchParams = useSearchParams();
-    const id = searchParams.get('id');
+    const documentId = searchParams.get('id');
     const [documentTitle, setDocumentTitle] = useState('');
     const [documentContent, setDocumentContent] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
-    const document = { title: documentTitle, content: documentContent };
+
+    const document = { 
+        title: documentTitle,
+        content: documentContent
+    };
+
+    const socket = useRef(null);
+
+    // useEffect handling socket
+    useEffect(() => {
+        socket.current = io('http://localhost:1337');
+
+        // send document id to the server-socket
+        socket.current.emit('my-create-room', documentId);
+
+        socket.current.on('document-content-change', ({ content }) => {
+            setDocumentContent(content);
+        });
+
+        socket.current.on('document-title-change', ({ title }) => {
+            setDocumentTitle(title);
+        });
+        // triggered on unmount
+        return () => {
+            socket.current.disconnect();
+        }
+        
+    }, [documentId]);
 
 
     useEffect(() => {
         let errorFetching = false;
-        if (id) {
-            fetchDocument(id).then(({title, content}) => { 
+        if (documentId) {
+            fetchDocument(documentId).then(({title, content}) => { 
                 setDocumentTitle(title);
                 setDocumentContent(content); 
             }).catch(() => {
@@ -26,14 +55,20 @@ export default function EditDocument() {
                 setErrorMessage(`Failed to fetch document`);
             });
         }
-        }, [id]);
+        }, [documentId]);
 
     const handleTitleChange = (event) => {
-        setDocumentTitle(event.target.value);
+        const title = event.target.value;
+        setDocumentTitle(title);
+        socket.current.emit('document-title-change', {documentId, title});
     };
 
     const handleContentChange = (event) => {
-        setDocumentContent(event.target.value);
+        const content = event.target.value;
+        // update locally
+        setDocumentContent(content);
+        // send to server
+        socket.current.emit('document-content-change', {documentId, content});
     };
 
     if (!document) return <div>Loading...</div>;
@@ -64,7 +99,7 @@ export default function EditDocument() {
             fullWidth
         />
         </Box>
-        <CustomizedMenuBtn data={ { 'document': document, 'id': id } } />
+        <CustomizedMenuBtn data={ { 'document': document, 'documentId': documentId } } />
     </Box>
     );
 }
