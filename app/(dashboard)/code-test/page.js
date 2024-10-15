1e599b57fda328efc49ef83381726fd4966d9d37
Martin <maru23@student.bth.se>
2024-10-15 11:34:15 +0200

    Updated code mode to handle changed back and forward

--- app/(dashboard)/code-test/page.js
+++ app/(dashboard)/code-test/page.js
@@ -1,11 +1,32 @@
 'use client';
 import CodeMode from "@/components/code-mode/CodeMode";
+import { useState } from "react";
+import TextField from '@mui/material/TextField';
 
 export default function CodeTest() {
+
+    const [code, setCode] = useState("console.log('Hello World')");
+    const [title, setTitle] = useState("Title");
+
+    async function codeChanged(code, event) {
+        console.log('code changed', code);
+    }
+
+    async function titleChanged(title, event) {
+        console.log('title changed', title);
+    }
+
     return (
         <div>
-            <h1>Code Test</h1>
-            <CodeMode code="console.log('Hello World')" onSave={code => alert(`The code is:\n${code}`)} />
+            <h1>Code editor test page</h1>
+            <TextField fullWidth label="Update code external" multiline rows={4} value={code} onChange={event => setCode(event.target.value)} />
+            <TextField fullWidth label="Update Title external" value={title} onChange={e => {console.log(e.target.value);setTitle(e.target.value)}} />
+            
+            <hr />
+            Nedanför är komponenten för editorn, ovanför test för att uppdatera
+            <hr />
+
+            <CodeMode code={code} onSave={code => alert(`The code is:\n${code}`)} onChange={codeChanged} title={title} onTitleChange={titleChanged} />
         </div>
     )
 }
--- components/code-mode/CodeMode.js
+++ components/code-mode/CodeMode.js
@@ -1,11 +1,12 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect, use } from 'react';
 import Editor from '@monaco-editor/react';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import LoadingButton from '@mui/lab/LoadingButton';
 import SendIcon from '@mui/icons-material/Send';
 import SaveIcon from '@mui/icons-material/Save';
+import TextField from '@mui/material/TextField';
 
 
 /**
@@ -14,35 +15,48 @@ import SaveIcon from '@mui/icons-material/Save';
  * @param {Object} props - The props object of the component.
  * @param {string} props.code - The code to display in the editor.
  * @param {Function} props.onSave - The function to call when the save button is clicked.
+ * @param {Function} props.onChange - The function to call when the code changes. (value, event) => void
  * @param {string} [props.title='Title'] - The title of the code editor.
+ * @param {Function} [props.onTitleChange] - The function to call when the title changes. (title, event) => void
  * @returns {JSX.Element} - The JSX element to render.
  * @example
  * <CodeMode code="console.log('Hello World')" onSave={code => alert(`The code is:\n${code}`)} />
  */
-export default function CodeMode({ code, onSave, title = 'Title' }) {
+export default function CodeMode({ code, onSave, onChange, title = 'Title', onTitleChange }) {
     const [loadingCode, setLoadingCode] = useState(false);
     const [loadingSave, setLoadingSave] = useState(false);
+    const [internalTitle, setInternalTitle] = useState(title);
     const editorRef = useRef(null);
+    
+
+
+    useEffect(() => {
+        console.log('Title changed', title);
+        setInternalTitle(title);
+    }, [title]);
 
     async function saveCode() {
-        if (onSave) {
-            try {
-                setLoadingSave(true);
-                await onSave(editorRef.current.getValue(), title);                    
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoadingSave(false);
-            }
-        } else {
+        if (!onSave) {
             alert('Save not implemented');
+            return;
+        };
+
+        try {
+            setLoadingSave(true);
+            await onSave(editorRef.current.getValue(), title);                    
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoadingSave(false);
         }
+        
     };
 
     async function runCode(event) {
-        setLoadingCode(true);
-        const code = editorRef.current.getValue();
         try {
+            setLoadingCode(true);
+            const code = editorRef.current.getValue();
+
             console.log(code);
             const response = await fetch('https://execjs.emilfolino.se/code', {
                 method: 'POST',
@@ -54,7 +68,7 @@ export default function CodeMode({ code, onSave, title = 'Title' }) {
             const data = await response.json();
             const returnValue = atob(data.data);
             console.log(returnValue);
-            setLoadingCode(false);
+
             alert(returnValue);
         }
         catch (error) {
@@ -62,6 +76,14 @@ export default function CodeMode({ code, onSave, title = 'Title' }) {
         }
         finally {
             setLoadingCode(false);
+        }
+    }
+
+    function titleChange(event) {
+        setInternalTitle(event.target.value);
+        
+        if (onTitleChange) {
+            onTitleChange(event.target.value, event);
         }
     }
 
@@ -80,19 +102,18 @@ export default function CodeMode({ code, onSave, title = 'Title' }) {
                     endIcon={<SaveIcon />}
                     loading={loadingSave}
                     onClick={saveCode}>Save</LoadingButton>
-                <LoadingButton disabled style={{
-                display: 'inline-block', margin: 0, paddingInline: '.5em', textOverflow: 'ellipsis',
-                whiteSpace: 'nowrap', overflow: 'hidden'
-            }}>{title}</LoadingButton>
-
+                
+            <TextField label="Title" value={internalTitle} fullWidth onChange={titleChange} />
             </ButtonGroup>
 
             <Editor
                 height="85vh"
                 defaultLanguage="javascript"
                 defaultValue={code}
+                value={code}
                 theme='vs-dark'
                 onMount={(editor, monaco) => editorRef.current = editor}
+                onChange={onChange}
             />
         </>
     )
